f47a497642d0b18427ba8be8c044cda695161672
William M <cominatyou@cominatyou.com>
2021-06-24 18:07:39 -0500

    Use JSON for config

--- src/main/java/com/cominatyou/App.java
+++ src/main/java/com/cominatyou/App.java
@@ -7,6 +7,7 @@ import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.TrayIcon.MessageType;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -21,20 +22,31 @@ import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.emoji.CustomEmojiBuilder;
 import org.javacord.api.entity.emoji.KnownCustomEmoji;
 import org.javacord.api.entity.server.Server;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class App {
     static void displayNotification(String title, String description, TrayIcon trayIcon, MessageType type) throws AWTException {
         trayIcon.displayMessage(title, description, type);
     }
-    public static void main(String[] argv) throws AWTException, IOException, InterruptedException {
-        DiscordApi api = new DiscordApiBuilder().setToken(Config.token).login().join();
+    public static void main(String[] argv) throws AWTException, IOException, InterruptedException, ParseException {
+        final TrayIcon trayIcon = SystemTray.isSupported() ? WinTrayIcon.createTrayIcon() : null;
+        JSONObject config = (JSONObject) new JSONParser().parse(new FileReader("./config.json"));
+        DiscordApi api = null;
+        try {
+            api = new DiscordApiBuilder().setToken((String) config.get("token")).login().join();
+        }
+        catch (Exception e) {
+            displayNotification("Unable to Connect to Discord", "We couldn't connect to Discord. This is most likely caused by an incorrect token.", trayIcon, MessageType.ERROR);
+            System.exit(1);
+        }
         final String folderPath = "\\\\UbuntuNAS\\CDN\\DiscordEmotes";
         final boolean suppressNotifications = !SystemTray.isSupported();
-        final TrayIcon trayIcon = SystemTray.isSupported() ? WinTrayIcon.createTrayIcon() : null;
         Path dir = Paths.get(folderPath);
         Server server = null;
         try {
-            server = api.getServerById(Config.serverID).get();
+            server = api.getServerById((String) config.get("serverID")).get();
         }
         catch (Exception e) {
             displayNotification("Invalid Server ID", "Unable to access the provided server ID. Make sure that the bot is in the server, and that the ID is correct.", trayIcon, MessageType.ERROR);
@@ -48,7 +60,7 @@ public class App {
         WatchService watcher = FileSystems.getDefault().newWatchService();
         WatchKey key = null;
         try {
-            key = dir.register(watcher, ENTRY_CREATE);
+            key = dir.register(watcher, ENTRY_CREATE /*, ENTRY_DELETE */);
         }
         catch (IOException e) {
             System.err.println(e);
@@ -72,14 +84,13 @@ public class App {
                         if (!suppressNotifications) displayNotification("Invalid File", filename.toString() + " is not a recognized image type.", trayIcon, MessageType.ERROR);
                         continue;
                     }
-                    // else if (tempFile.length() > 256000 && !Files.probeContentType(child).equals("image/gif")) {
-                    //     if (!suppressNotifications) displayNotification("File Too Large", child.getFileName() + " is " + (double) tempFile.length() / 1000000d + " MB. The maximum file size is 256kb.", trayIcon, MessageType.ERROR);
-                    //     System.out.println("File delete status: " + tempFile.delete());
+                    // else if (child.toFile().length() > 256000 && !Files.probeContentType(child).equals("image/gif")) {
+                    //     if (!suppressNotifications) displayNotification("File Too Large", child.toFile().getName() + " is " + (double) child.toFile().length() / 1000000d + " MB. The maximum file size is 256kb.", trayIcon, MessageType.ERROR);
+                    //     System.out.println("File delete status: " + child.toFile().delete());
                     //     continue;
                     // }
                     else {
                         System.out.println(filename.toFile().getName() + " found!");
-                        // System.out.println("File delete status: " + tempFile.delete());
                     }
                 } catch (IOException e) {
                     System.err.println(e);
@@ -87,8 +98,6 @@ public class App {
                 File emojiImage = dir.resolve(filename).toFile();
                 String emojiName = emojiImage.getName().substring(0, emojiImage.getName().lastIndexOf('.'));
                 try {
-                    // IOException (file in use) gets thrown if I don't delay execution
-                    // There probably is a better way to handle this but I have already worked on this for five hours today, so this will suffice
                     Thread.sleep(500);
                     CustomEmojiBuilder emote = new CustomEmojiBuilder(server).setImage(emojiImage).setName(emojiName);
                     KnownCustomEmoji createdEmoji = null;
